a53fc2e274e182b599d001c87ee1af162402bd11
tamer <tmeraslan1@gmail.com>
2025-07-08 12:44:58 +0300

    is_unique soultion

--- katas/test/test_is_unique.py
+++ katas/test/test_is_unique.py
@@ -1,15 +1,15 @@
 import unittest
-from katas.is_unique import is_unique
+from katas.is_unique_str import is_unique_str
 
 
 class TestIsUnique(unittest.TestCase):
     def test_null_string(self):
-        self.assertEqual(is_unique(''), True)
+        self.assertEqual(is_unique_str(''), True)
     
     def test_uniqe_string(self):
-        self.assertEqual(is_unique('abcdef'), True)
+        self.assertEqual(is_unique_str('abcdef'), True)
 
     def test_not_uniqe_string(self):
-        self.assertEqual(is_unique('abedb'), False)
+        self.assertEqual(is_unique_str('abedb'), False)
 
 
